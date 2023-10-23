64eade0f3a270dab8d021d164b3adea5589553a7
Bootcamp74 <148359746+Bootcamp74@users.noreply.github.com>
2023-10-22 23:06:21 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+// Fetch response for city latatude and longitude
+
+requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=fc8bffadcdca6a94d021c093eac22797';
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 
 
 // Fetch response for weather
